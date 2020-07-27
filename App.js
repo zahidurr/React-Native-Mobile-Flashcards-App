5a866a9ab63ea8c17daa9791af8b8595b8e320ce
Zahidur <zahidur.r@outlook.com>
2020-07-28 00:09:41 +0200

    APK binary fix

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SplashScreen from 'react-native-splash-screen'
 import { Provider as PaperProvider } from "react-native-paper";
 import { Provider as StoreProvider } from "react-redux";
 import store from "./src/store";
 import Router from "./src/router";
 import { themestyle } from "./src/utils/themestyle";
 
-const Main = () => (
-  <StoreProvider store={store}>
-    <PaperProvider theme={themestyle}>
-      <Router />
-    </PaperProvider>
-  </StoreProvider>
-);
+const Main = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider theme={themestyle}>
+        <Router />
+      </PaperProvider>
+    </StoreProvider>
+  );
+}
 
 export default Main;
